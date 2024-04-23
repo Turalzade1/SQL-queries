6db7622f62b0ef16a06d3b00ddb2662e3e8b83cd
Turalzade1 <167862106+Turalzade1@users.noreply.github.com>
2024-04-23 19:29:42 +0300

    Delete Desktop/Database_Asministration/SQL_queries directory

--- Desktop/Database_Asministration/SQL_queries/AVG,MIN,MAX,SUM,COUNT/queries/customers for payment amount.sql
+++
@@ -1,6 +0,0 @@
--- customers which amount of payment more than 170
-SELECT c.customer_id, c.first_name, c.last_name, SUM(p.amount) AS total_payment
-FROM customer c
-JOIN payment p ON c.customer_id = p.customer_id
-GROUP BY c.customer_id, c.first_name, c.last_name
-HAVING SUM(p.amount) > 170;
